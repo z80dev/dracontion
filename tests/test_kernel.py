70ea8c85e061aaef9cf779f599f047bdcad04469
z80 <z80@ophy.xyz>
2022-12-16 14:54:54 -0500

    add counter example test

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -1,1 +1,14 @@
 import boa
+
+def test_counter_inc():
+    k = boa.load("contracts/Kernel.vy")
+    counts_module = boa.load("contracts/modules/Counts.vy", k.address)
+    counter_policy = boa.load("contracts/policies/Counter.vy", k.address)
+
+    k.executeAction(1, counts_module.address)
+    k.executeAction(4, counter_policy.address)
+
+    counter_policy.inc()
+
+    count = counts_module.counts(k.executor())
+    assert count == 1
